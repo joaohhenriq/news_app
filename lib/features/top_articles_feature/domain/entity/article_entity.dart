4b04af3043c9bec0764c590e5243b3e2ce66f1af
joaohhenriq <joao.camargo@avenuesec.io>
2024-07-14 23:32:01 -0300

    Adds articles domain tests

--- lib/features/top_articles_feature/domain/entity/article_entity.dart
+++ lib/features/top_articles_feature/domain/entity/article_entity.dart
@@ -25,14 +25,37 @@ class ArticleEntity extends Equatable {
 
   @override
   List<Object?> get props => [
-    sourceId,
-    sourceName,
-    author,
-    title,
-    description,
-    url,
-    urlToImage,
-    publishedAt,
-    content,
-  ];
+        sourceId,
+        sourceName,
+        author,
+        title,
+        description,
+        url,
+        urlToImage,
+        publishedAt,
+        content,
+      ];
+
+  ArticleEntity copyWith({
+    String? sourceId,
+    String? sourceName,
+    String? author,
+    String? title,
+    String? description,
+    String? url,
+    String? urlToImage,
+    String? publishedAt,
+    String? content,
+  }) =>
+      ArticleEntity(
+        sourceId: sourceId ?? this.sourceId,
+        sourceName: sourceName ?? this.sourceName,
+        author: author ?? this.author,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        url: url ?? this.url,
+        urlToImage: urlToImage ?? this.urlToImage,
+        publishedAt: publishedAt ?? this.publishedAt,
+        content: content ?? this.content,
+      );
 }
